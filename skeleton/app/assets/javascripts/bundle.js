1d1ab219a7de61d2944a9ec1404b5551a5cbac12
Sam Wang <sam.zz.wang@gmail.com>
2017-05-03 13:46:45 -0700

    add followtoggle click handler

--- skeleton/app/assets/javascripts/bundle.js
+++ skeleton/app/assets/javascripts/bundle.js
@@ -87,14 +87,31 @@ $(() => {
 class FollowToggle {
   constructor ($el) {
     this.$el = $el;
-    this.userId = $el.find("data-user-id");
-    this.followState = $el.find("data-initial-follow-state");
+    this.userId = $el.attr("data-user-id");
+    this.followState = $el.attr("data-initial-follow-state");
     this.render();
+    $el.on("click", this.handleClick.bind(this));
   }
 
   render () {
-    this.$el.text(this.followState === "followed" ? "Follow!" : "Unfollow!");
+    this.$el.text(this.followState === "followed" ? "Unfollow!" : "Follow!");
   }
+
+  handleClick (e) {
+    e.preventDefault();
+    const meth = this.followState === "followed" ? 'delete' : 'post';
+    const that = this;
+    $.ajax( {
+      method: meth,
+      url: `/users/${this.userId}/follow`,
+      dataType: 'json',
+      success: () => {
+        this.followState = this.followState === "followed" ? "unfollowed" : "followed";
+        this.render();
+      }
+    });
+  }
+
 }
 
 module.exports = FollowToggle;
